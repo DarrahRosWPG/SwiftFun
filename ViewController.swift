fce17b10fc087493f566d8fc97a7b623648a63a4
Charles Darrah Ros <darrahros@wpgmail.com>
2018-03-08 17:17:01 -0500

    A Janky Calculator

--- ViewController.swift
+++ ViewController.swift
@@ -14,10 +14,29 @@ class ViewController: UIViewController {
     var buttonCount = 0
     
     @IBOutlet weak var MyLabel: UILabel!
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
+    @IBOutlet weak var additionSwitch: UISwitch!
     
     @IBAction func buttonTapped(_ sender: Any) {
         
-        buttonCount = buttonCount + 1
+        let addition = additionSwitch.isOn
+        
+        if addition {
+            let sum = Double(topTextField.text!)! + Double(bottomTextField.text!)!
+            
+            //MyLabel.text = "The sum is " + String(sum)
+            //MyLabel.text = "The sum is \(sum)"
+            MyLabel.text = "\(topTextField.text!) + \(bottomTextField.text!) = \(sum)"
+        } else {
+            let sum = Double(topTextField.text!)! - Double(bottomTextField.text!)!
+            
+            //MyLabel.text = "The sum is " + String(sum)
+            //MyLabel.text = "The sum is \(sum)"
+            MyLabel.text = "\(topTextField.text!) - \(bottomTextField.text!) = \(sum)"
+        }
+        
+        /*buttonCount = buttonCount + 1
         print(buttonCount)
         
         if buttonCount == 10 {
@@ -26,7 +45,7 @@ class ViewController: UIViewController {
             MyLabel.text = "Darrah is Cool"
             buttonCount = 0
             
-        }
+        } */
         
     }
     
